2393e058870ca2791e402a6438ead9d820e30732
HerculesOliv <herculesdeoliveiraveloso@gmail.com>
2024-03-21 11:44:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, {useStats, useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  const [numero1, setnumero1] = useState(0);
+  const [numero2, setnumero2] = useState(0);
+  const [resultado, setResultado] = useState(0);
+  const [operacao, setoperacao] = useState('somar');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Calculadora React</h1>
+      <label>Primeiro Numero</label>
+      <input 
+        type="number"
+        value={numero1}
+        onChange={(0) => setnumero1(e.target.value)} />
+      
+      <label>Segundo Numero</label>
+      <input 
+        type="number"
+        value={numero2}
+        onChange={(0) => setnumero2(e.target.value)} />
+
+      <select>
+        <option>Somar</option>
+        <option>Subtrair</option>
+        <option>MUltiplicar</option>
+        <option>dividir</option>
+      </select>
+
+      <label>Resultado 0</label>
     </div>
   );
 }
